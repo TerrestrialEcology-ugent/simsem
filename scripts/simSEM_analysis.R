d3ef5dd35b7c52d05fed91bfc8d02200ddde59f1
lhertzog <7189850+lionel68@users.noreply.github.com>
2019-01-07 14:46:21 +0100

    revised version script

---
+++ scripts/simSEM_analysis.R
@@ -0,0 +1,210 @@
+##################
+
+# R script to plot the results from the simulations from the submitted manuscript:
+
+# How robust are Structural Equation Models to model mis-specification? A simulation study
+
+# author: Lionel Hertzog
+
+# date: 06.01.2019
+
+########
+
+# note that you can re-run the simulations on your machine using the scripts simSEM_simulations.R and simSEM_function.R, you can also download the data used in the manuscript from: URL link
+
+## load the libraries
+library(tidyverse)
+
+## set the wd, point to where the github repo was cloned
+setwd("~/Desktop/Blog/GitFolder/SimSEM/")
+
+## first simulation batch
+
+# load back the simulation data
+res_pre <- read.csv("data/simSem_res_pre_rev.csv", sep = " ")
+
+# aggregate over the replications per parameter set
+res_pre %>%
+  group_by(pkg,type,Exp,N,X,Sd_eff,Sd_res)%>%
+  dplyr::summarise(PropSigM=sum(sigM,na.rm=TRUE)/n(),NbPaths = sum(nbPaths),NbSigPaths=sum(nbSigPaths),
+            AvgRSq=median(avgRsq,na.rm=TRUE),PropCond = mean(Local,na.rm=TRUE))%>%
+  dplyr::mutate(PropSigPaths=NbSigPaths/NbPaths) %>%
+  dplyr::mutate(Signal = paste0("sd_eff: ",Sd_eff,"\nsd_res: ",Sd_res)) -> res_all
+
+# the main figures:
+
+## fig.1 variation in proportion of accepted models
+gg1 <- ggplot(subset(res_all,(Sd_res == 1 & Sd_eff == 2.5) | (type == "random" & Sd_res == 1)),aes(x=N,y=PropSigM,color=type,linetype=pkg))+geom_path()+
+  facet_grid(~X) +
+  scale_x_log10() +
+  labs(x="Sample size",y="Proportion of accepted models (p > 0.05)",
+       title="") +
+  scale_linetype_discrete(name = "package") +
+  scale_color_discrete(name= "data generation\nscenario")
+
+ggsave("figures/prop_acc_rev.png",gg1,width=30,height=15,unit="cm",dpi=100)
+# discuss better detecting power for pcsem as model get more complex for low sample size
+
+## fig.2 variation in proportion of accepted paths
+gg2 <- ggplot(subset(res_all,(Sd_res == 1 & Sd_eff == 2.5) | (type == "random" & Sd_res == 1)),aes(x=N,y=PropSigPaths,color=type,linetype=pkg))+geom_path()+
+  facet_grid(~X) +
+  scale_x_log10() +
+  geom_hline(yintercept = 0.8,linetype="dashed") +
+  labs(x="Sample size",y="Proportion of significant paths (p < 0.05)",
+       title="") +
+  scale_linetype_discrete(name = "package") +
+  scale_color_discrete(name= "data generation\nscenario")
+
+ggsave("figures/prop_path_rev.png",gg2,width=30,height=15,unit="cm",dpi=100)
+# discuss the heuristic of good power to detect sig paths for 10 data points per paths
+
+## fig.3 variation in average R-square
+gg3 <- ggplot(subset(res_all,Sd_res == 1 & Sd_eff == 2.5),aes(x=N,y=AvgRSq,color=type,linetype=pkg))+geom_path()+
+  facet_grid(~X) +
+  scale_x_log10() +
+  labs(x="Sample size",y="Average R-square of the constituing models",
+       title="") +
+  scale_linetype_discrete(name = "package") +
+  scale_color_discrete(name= "data generation\nscenario")
+
+ggsave("figures/avg_rsq_rev.png",gg3,width=30,height=15,unit="cm",dpi=100)
+#
+## fig.4 variation in proportion of failed conditional independence tests
+gg4 <- ggplot(subset(res_all,(Sd_res == 1 & Sd_eff == 2.5) | (type == "random" & Sd_res == 1)),aes(x=N,y=PropCond,color=type,linetype=pkg))+geom_path()+
+  facet_grid(~X) +
+  scale_x_log10() +
+  labs(x="Sample size",y="Average proportion of breached conditional independence",
+       title="") +
+  scale_linetype_discrete(name = "package") +
+  scale_color_discrete(name= "data generation\nscenario")
+
+ggsave("figures/prop_cond_rev.png",gg4,width=30,height=15,unit="cm",dpi=100)
+
+#
+# # appendix figures
+# ## this time looking across signal / noise ratios
+#
+gg1 <- ggplot(res_all,aes(x=N,y=PropSigM,color=type,linetype=pkg))+geom_path()+
+  facet_grid(X~Signal) +
+  scale_x_log10() +
+  labs(x="Sample size",y="Proportion of accepted models (p > 0.05)",
+       title="Proportion of accepted model for different data generation") +
+  scale_linetype_discrete(name = "package") +
+  scale_color_discrete(name= "data generation\nscenario")
+
+ggsave("figures/prop_acc_A_rev.png",gg1,width=30,height=15,unit="cm",dpi=100)
+
+gg2 <- ggplot(res_all,aes(x=N,y=PropSigPaths,color=type,linetype=pkg))+geom_path()+
+  facet_grid(X~Signal) +
+  scale_x_log10() +
+  geom_hline(yintercept = 0.8,linetype="dashed") +
+  labs(x="Sample size",y="Proportion of significant paths (p < 0.05)",
+       title="Proportion of significant paths for different data generation") +
+  scale_linetype_discrete(name = "package") +
+  scale_color_discrete(name= "data generation\nscenario")
+
+ggsave("figures/prop_path_A_rev.png",gg2,width=30,height=15,unit="cm",dpi=100)
+# #
+gg3 <- ggplot(subset(res_all,type!="random"),aes(x=N,y=AvgRSq,color=type,linetype=pkg))+geom_path()+
+  facet_grid(X~Signal) +
+  scale_x_log10() +
+  labs(x="Sample size",y="Average R-square of the constituing models",
+       title="Average R-square for different data generation")  +
+  scale_linetype_discrete(name = "package") +
+  scale_color_discrete(name= "data generation\nscenario")
+#
+ggsave("figures/avg_rsq_A_rev.png",gg3,width=30,height=15,unit="cm",dpi=100)
+# #
+gg4 <- ggplot(res_all,aes(x=N,y=PropCond,color=type,linetype=pkg))+geom_path()+
+  facet_grid(X~Signal) +
+  scale_x_log10() +
+  labs(x="Sample size",y="Average proportion of breached conditional independence",
+       title="Average failed conditional independence for different data generation")  +
+  scale_linetype_discrete(name = "package") +
+  scale_color_discrete(name= "data generation\nscenario")
+
+ggsave("figures/prop_cond_A_rev.png",gg4,width=30,height=15,unit="cm",dpi=100)
+#
+
+## second simulation batch
+
+# load back saved data
+res_pre <- read.csv("data/res_pre_c2.csv",sep=" ")
+
+# the parameter set used to generate the simulations
+sims_c <- expand.grid(N = c(seq(20,100,20),200,500,1000,5000,10000),
+                    X = c(5,7,10),
+                    C = 0.3,
+                    p_var = 0.25,
+                    pkg = c("lavaan","pcsem"),
+                    sd_eff = c(1, 2.5, 5),
+                    sd_res = c(0.5, 1, 2))
+# add an ID to each line
+sims_c$Exp <- 1:nrow(sims_c)
+
+n_rep <- 100
+
+res_pre %>%
+  mutate(rep = 1:n()) %>% # add an unique ID for each replicate
+  gather("Type","value",AIC_t:hbic_o) %>% # melt
+  separate(Type,c("metric","scenario")) %>% # separate IC metric from the scenario
+  group_by(rep,N,X,sd_eff,sd_res,C,p_var,metric) %>% # group by replicate ID
+  mutate(min = min(value)) %>% # the minimum value per replicate and metric
+  mutate(is_small = if_else(abs(value - min) < 2,1,0)) %>% # if the IC value is within 2 unit of the smallest one mark as 1
+  summarise(best_scenario = if_else(sum(is_small) == 1,scenario[which.min(value)],"none")) %>% # if there is only one smallest IC value get the scenario
+  ungroup() %>%
+  mutate(Exp = rep(1:nrow(sims_c),each = 3 * n_rep)) %>% # re-add the Exp ID
+  group_by(Exp, N, X, sd_eff, sd_res, C, p_var, metric) %>% # group by Exp ID (ie rows in the sims_c dataframe)
+  count(best_scenario) %>% # count how often each scenario pops out across the replicate within Exp ID
+  mutate(prop = n / sum(n)) %>%
+  left_join(sims_c[,c("Exp","pkg")],by="Exp") %>% # get the proportion
+  mutate(Signal = paste0("sd_eff: ",sd_eff,"\nsd_res: ",sd_res)) -> dd 
+
+dd$best_scenario <- factor(dd$best_scenario,labels = c("none","underspecified","shuffled","exact","overspecified"))
+
+## figure 5. proportion of replicates where each scenario was labelled best scenario
+dd_all <- subset(dd, sd_eff ==2.5 & sd_res == 1 & !is.na(dd$best_scenario))
+dd_all$metric[dd_all$metric == "AIC"] <- "AICc"
+
+gg_all <- ggplot(dd_all,aes(x=N,y=prop,color=best_scenario,linetype=pkg)) +
+  geom_line() +
+  facet_grid(X~metric) +
+  scale_x_log10(breaks=c(20,50,100,1000,10000)) +
+  labs(title="",x="Sample size",y="Proportion of simulations") +
+  scale_linetype_discrete(name="package") +
+  scale_color_discrete(name="best scenario") +
+  theme(panel.grid.minor = element_blank())
+
+ggsave("figures/ic_metric_prop.png",gg_all,width=10,height=8)
+
+## appendix figures looking across signal / noise ratio for the different IC metrics separately
+dd_aic <- subset(dd,metric=="AIC" & !is.na(best_scenario))
+
+gg_aic <- ggplot(dd_aic,aes(x=N,y=prop,color=best_scenario,linetype=pkg)) +
+  geom_line() +
+  facet_grid(X~Signal) +
+  scale_x_log10() +
+  labs(title="AICc",x="Sample size",y="Proportion of simulations")
+
+ggsave("figures/aic_appendix.png",gg_aic, width = 10, height = 8)
+
+
+dd_bic <- subset(dd,metric=="BIC" & !is.na(best_scenario))
+
+gg_bic <- ggplot(dd_bic,aes(x=N,y=prop,color=best_scenario,linetype=pkg)) +
+  geom_line() +
+  facet_grid(X~Signal) +
+  scale_x_log10() +
+  labs(title="BIC",x="Sample size",y="Proportion of simulations")
+
+ggsave("figures/bic_appendix.png",gg_bic,height=8,width=10)
+
+dd_hbic <- subset(dd,metric=="hbic" & !is.na(best_scenario))
+
+gg_hbic <- ggplot(dd_hbic,aes(x=N,y=prop,color=best_scenario,linetype=pkg)) +
+  geom_line() +
+  facet_grid(X~Signal) +
+  scale_x_log10() +
+  labs(title="HBIC",x="Sample size",y="Proportion of simulations")
+
+ggsave("figures/hbic_appendix.png",gg_hbic, width = 10, height = 8)
--- scripts/simSEM_function.R
+++ scripts/simSEM_function.R
@@ -68,9 +68,8 @@ model_list <- function(formL,data,fn=NULL){
 }
 
 #create a function to get important infos from a sem object
-grab_val <- function(modL,data,lavaan=FALSE){
-  if(lavaan){
-    lv <- "lavaan"
+grab_val <- function(modL,data,pkg="pcsem"){
+  if(pkg == "lavaan"){
     semObj <- tryCatch(sem.lavaan(modL,data),error = function(e) NA)
     if(! is.na(semObj)){
       sigM <-ifelse(lavInspect(semObj,"converged"),ifelse(lavInspect(semObj,"fit")["pvalue"]>=0.05,1,0),0) #get the SEM p-value, giving a value of 0 if the model did not converged
@@ -92,26 +91,36 @@ grab_val <- function(modL,data,lavaan=FALSE){
     }
   }
   else{
-    lv <- "pcSEM"
-    semObj <- sem.fit(modL,data,.progressBar = FALSE,conditional = TRUE) #evaluate the independence claims
-    sigM <-ifelse(semObj$Fisher.C$p.value>=0.05,1,0) #get the SEM p-value
-    semCoeff <- sem.coefs(modL,data) #get the model coefficients
-    nbPaths <- nrow(semCoeff)
-    nbSigPaths <- length(which(semCoeff$p.value<=0.05)) #how many significant paths
-    avgRsq <- mean(laply(modL,function(x) summary(x)$adj.r.squared))
-    #avgSigPaths <- mean(abs(semCoeff$estimate[semCoeff$p.value<=0.05])) #the average values of significant paths
-    aic <- sem.aic(modL,data,.progressBar = FALSE)$AICc
-    bic <- semObj$Fisher.C[1] + semObj$AIC["K"] * log(semObj$AIC["n"])
-    hbic <- semObj$Fisher.C[1] + log(nrow(data) / (2*pi)) * semObj$AIC["K"]
+    semObj <- tryCatch(sem.fit(modL,data,.progressBar = FALSE,conditional = TRUE),error=function(e) NA) #evaluate the independence claims
+    if(!is.na(semObj)){
+      sigM <-ifelse(semObj$Fisher.C$p.value>=0.05,1,0) #get the SEM p-value
+      semCoeff <- sem.coefs(modL,data) #get the model coefficients
+      nbPaths <- nrow(semCoeff)
+      nbSigPaths <- length(which(semCoeff$p.value<=0.05)) #how many significant paths
+      avgRsq <- mean(laply(modL,function(x) summary(x)$adj.r.squared))
+      #avgSigPaths <- mean(abs(semCoeff$estimate[semCoeff$p.value<=0.05])) #the average values of significant paths
+      aic <- sem.aic(modL,data,.progressBar = FALSE)$AICc
+      bic <- semObj$Fisher.C[1] + semObj$AIC["K"] * log(semObj$AIC["n"])
+      hbic <- semObj$Fisher.C[1] + log(nrow(data) / (2*pi)) * semObj$AIC["K"]
+    }
+    else{
+      sigM <- NA
+      nbPaths <- NA
+      nbSigPaths <- NA
+      avgRsq <- NA
+      aic <- bic <- hbic <- NA
+    }
   }
   out <- data.frame(N=nrow(data),X=ncol(data),sigM=sigM,nbPaths=nbPaths,nbSigPaths=nbSigPaths,avgRsq=avgRsq,AIC=aic,BIC=as.numeric(bic),hbic=as.numeric(hbic))
   return(out)
 }
 
 #a function to generate data based on diverse fidelity from the simulated list of models
-generate_data <- function(relL,formL,N=20,X=5,type="random",p_var=0.25,sd_eff=2.5,sd_res=1){
+generate_data <- function(relL,N=20,X=5,type="random",p_var=0.25,sd_eff=2.5,sd_res=1, out_dat = TRUE){
+  formL <- formula_list(relL)
   if(type=="random"){
     dat <- as.data.frame(matrix(rnorm(X*N,0,sd_res),ncol=X,dimnames = list(1:N,paste0("X",1:X))))
+    relL_m <- relL
   }
   else if(type=="exact"){
     dat <- data.frame(X1=runif(N,-2,2))
@@ -120,10 +129,11 @@ generate_data <- function(relL,formL,N=20,X=5,type="random",p_var=0.25,sd_eff=2.
       y <- allv[1]
       xs <- dat[,allv[2:length(allv)]]
       xs <- as.matrix(cbind(rep(1,N),xs))
-      coeff <- rcauchy(n = ncol(xs),0,sd_eff)
+      coeff <- rnorm(n = ncol(xs),0,sd_eff)
       dat<-cbind(dat,rnorm(N,xs%*%coeff,sd_res))
       colnames(dat)[ncol(dat)]<-y
       dat <- as.data.frame(apply(dat,2,scale))
+      relL_m <- relL
     }
   }
   #reverse a certain proportion of the causal relation, such as X1 -> X2 becomes X2 -> X1
@@ -156,10 +166,10 @@ generate_data <- function(relL,formL,N=20,X=5,type="random",p_var=0.25,sd_eff=2.
     }
     #sort back the DAG
     od <- topo_sort(graph_from_adjacency_matrix(relL_m),mode="in")
-    relL_mo <- relL_m[od,od]
-    formL_m <- formula_list(relL_mo)
+    relL_m <- relL_m[od,od]
+    formL_m <- formula_list(relL_m)
     #a random vector for the exogenous variable(s) to start of the data creation
-    exo <- which(rowSums(relL_mo) == 0)
+    exo <- which(rowSums(relL_m) == 0)
     dat <- as.data.frame(matrix(runif(N * length(exo),-2,2),ncol=length(exo)))
     names(dat) <- names(exo)
     for(f in 1:length(formL_m)){
@@ -167,7 +177,7 @@ generate_data <- function(relL,formL,N=20,X=5,type="random",p_var=0.25,sd_eff=2.
       y <- allv[1]
       xs <- dat[,allv[2:length(allv)]]
       xs <- as.matrix(cbind(rep(1,N),xs))
-      coeff <- rcauchy(n = ncol(xs),0,sd_eff)
+      coeff <- rnorm(n = ncol(xs),0,sd_eff)
       dat<-cbind(dat,rnorm(N,xs%*%coeff,sd_res))
       colnames(dat)[ncol(dat)]<-y
       dat <- as.data.frame(apply(dat,2,scale))
@@ -200,7 +210,7 @@ generate_data <- function(relL,formL,N=20,X=5,type="random",p_var=0.25,sd_eff=2.
       y <- allv[1]
       xs <- dat[,allv[2:length(allv)]]
       xs <- as.matrix(cbind(rep(1,N),xs))
-      coeff <- rcauchy(n = ncol(xs),0,sd_eff)
+      coeff <- rnorm(n = ncol(xs),0,sd_eff)
       dat<-cbind(dat,rnorm(N,xs%*%coeff,sd_res))
       colnames(dat)[ncol(dat)]<-y
       dat <- as.data.frame(apply(dat,2,scale))
@@ -224,7 +234,7 @@ generate_data <- function(relL,formL,N=20,X=5,type="random",p_var=0.25,sd_eff=2.
       y <- allv[1]
       xs <- dat[,allv[2:length(allv)]]
       xs <- as.matrix(cbind(rep(1,N),xs))
-      coeff <- rcauchy(n = ncol(xs),0,sd_eff)
+      coeff <- rnorm(n = ncol(xs),0,sd_eff)
       dat<-cbind(dat,rnorm(N,xs%*%coeff,sd_res))
       colnames(dat)[ncol(dat)]<-y
       dat <- as.data.frame(apply(dat,2,scale))
@@ -233,40 +243,98 @@ generate_data <- function(relL,formL,N=20,X=5,type="random",p_var=0.25,sd_eff=2.
   else{
     stop("Not implemented yet") #one could create datasets with non-random structure, to be implemented
   }
-  return(dat)
+  if(out_dat){
+    return(dat)
+  }
+  else{
+    return(formula_list(relL_m))
+  }
 }
 
 
 #put it all in one function to replicate over
-sim_sem <- function(N=20,X=5,C=0.3,type="random",p_var=0.25,lv=FALSE,sd_eff = 2.5,sd_res = 1){
+sim_sem <- function(N=20,X=5,C=0.3,type="random",p_var=0.25,pkg="lavaan",sd_eff = 2.5,sd_res = 1){
   relL <- relation(X=X,C=C)
   formL <- formula_list(relL)
   dagg <- to_dagitty(relL) #get DAG
-  dat <- generate_data(N=N,X=X,relL=relL,formL=formL,type=type,p_var=p_var,sd_eff=sd_eff,sd_res=sd_res)
+  dat <- generate_data(N=N,X=X,relL=relL,type=type,p_var=p_var,sd_eff=sd_eff,sd_res=sd_res)
   modL <- model_list(formL,dat)
-  out <- grab_val(modL,dat,lavaan=lv)
+  out <- grab_val(modL,dat,pkg=pkg)
   #get proportion of conditional independence violated
   ll <- localTests(dagg,dat)
   out$Local <- sum(ll$p.value < 0.05) / length(ll$p.value)
   out$Sd_eff <- sd_eff
   out$Sd_res <- sd_res
+  out$pkg <- pkg
+  out$type <- type
+  out$C <- C
   return(out)
+}
+
+# a new simsem function to truly compare models, step 1: generate exact data, step 2: create shuffled / under or overspecified model
+# potentially with varying p_var, step 3: grab_val on actual model_list and on model_list generated from shuffled / under/over specified model
+
+sim_sem_c <- function(N = 20, X = 5, C = 0.3, p_var = 0.25, pkg = "pcsem", sd_eff = 2.5, sd_res = 1){
+  relL <- relation(X=X,C=C)
+  formL <- formula_list(relL)
+  
+  # exact data generation
+  dat <- generate_data(N=N,X=X,relL=relL,type="exact",p_var=p_var,sd_eff=sd_eff,sd_res=sd_res)
+  # generate wrong formLs
+  formL_s <- generate_data(N=N,X=X,relL=relL,type="shuffled",p_var=p_var,sd_eff=sd_eff,sd_res=sd_res,out_dat=FALSE)
+  formL_u <- generate_data(N=N,X=X,relL=relL,type="underspecified",p_var=p_var,sd_eff=sd_eff,sd_res=sd_res,out_dat=FALSE)
+  formL_o <- generate_data(N=N,X=X,relL=relL,type="overspecified",p_var=p_var,sd_eff=sd_eff,sd_res=sd_res,out_dat=FALSE)
+  # true modL
+  modL_t <- model_list(formL,dat)
+  #wrong modL
+  modL_s <- model_list(formL_s, dat)
+  modL_u <- model_list(formL_u, dat)
+  modL_o <- model_list(formL_o, dat)
+  
+  # grab vals
+  out_t <- grab_val(modL_t,dat,pkg=pkg)
+  out_s <- grab_val(modL_s,dat,pkg=pkg)
+  out_u <- grab_val(modL_u,dat,pkg=pkg)
+  out_o <- grab_val(modL_o,dat,pkg=pkg)
+  
+  # put IC together
+  out_f <- data.frame(N=N,X=X,sd_eff=sd_eff,sd_res=sd_res,C=C,p_var=p_var,
+                      AIC_t=out_t$AIC,AIC_s=out_s$AIC,AIC_u=out_u$AIC,AIC_o=out_o$AIC,
+                      BIC_t=out_t$BIC,BIC_s=out_s$BIC,BIC_u=out_u$BIC,
+                      BIC_o=out_o$BIC,hbic_t=out_t$hbic,hbic_s=out_s$hbic,
+                      hbic_u=out_u$hbic,hbic_o=out_o$hbic,stringsAsFactors = FALSE)
+  return(out_f)
 }
 
 #a function to loop through the parameters
 sim_for <- function(sims,n_rep=100){
   res <- NULL
   for(i in 1:nrow(sims)){
-    type <- sims[i,1]
-    x <- as.numeric(sims[i,-1])
-    tmp <- replicate(n_rep,sim_sem(N = x[1],X=x[2],C=x[3],type=type,lv=x[4], sd_eff = x[5], sd_res = x[6]))
+    tmp <- replicate(n_rep,sim_sem(N = sims[i,"N"],X=sims[i,"X"],
+                                   C=sims[i,"C"],type=sims[i,"type"],
+                                   pkg=sims[i,"pkg"], sd_eff = sims[i,"sd_eff"], 
+                                   sd_res = sims[i,"sd_res"]))
     out <- as.data.frame(matrix(unlist(tmp),nrow = n_rep,byrow=TRUE,dimnames=list(1:n_rep,attr(tmp,"dimnames")[[2]])))
     out$Exp <- i
-    out$C <- x[3]
-    out$type <- type
-    out$pkg <- ifelse(x[4],"lavaan","pcsem")
     res <- rbind(res,out)
-    print(paste0(type," : ",i, " out of ",nrow(sims)))
+    print(paste0(sims[i,"type"]," : ",i, " out of ",nrow(sims)))
+  }
+  return(res)
+}
+
+# a second sim_for to re-do the model comparison
+# the sims object as this time no type column
+sim_for_c <- function(sims,n_rep=5){
+  res <- NULL
+  for(i in 1:nrow(sims)){
+    tmp <- replicate(n_rep,sim_sem_c(N = sims[i,"N"],X=sims[i,"X"],
+                                     C=sims[i,"C"],p_var=sims[i,"p_var"],
+                                     sd_eff = sims[i,"sd_eff"], 
+                                     sd_res = sims[i,"sd_res"]))
+    out <- as.data.frame(matrix(unlist(tmp),nrow = n_rep,byrow=TRUE,dimnames=list(1:n_rep,attr(tmp,"dimnames")[[2]])),stringsAsFactors = FALSE)
+    out$Exp <- i
+    res <- rbind(res,out)
+    print(paste0("Row : ",i, " out of ",nrow(sims)))
   }
   return(res)
 }
